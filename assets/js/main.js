95e31dd05d9bb96bfb8f46f64bedbda0f43bc4ae
LukyVj <lucas.bonomi@gmail.com>
2014-02-16 02:51:56 +0100

    edit demo

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,19 +8,52 @@ function bulltax(){
 	var $bull = $('pre.bulltax'),
 		$lang = $bull.find('code');
 
-		if($lang.hasClass('css')){
+		// UI fix
+		// It get the class of the children (code) to the parent class ($this = pre), so pre = pre.bulltax.html / pre.bulltax.css / pre.bulltax.js
+		$bull.each(function () {
+			var $this = $(this),
+			$childClass = $this.find('code').attr('class');
+			$this.addClass($childClass);
+		});
 
-			var $css = $lang,
+
+			// HTML syntax
+			var $lang = $('pre.bulltax code.html');
+			$html = $lang,
+			$htmlVal = $html.text(),
+			$a = $htmlVal
+			.replace('<','<span class="lt">&lt;</span>')
+			.replace('>','<span class="gt">&gt;</span>');
+
+			$html.empty().html($a);
+
+
+			// CSS syntax
+			var $lang = $('pre.bulltax code.css');
+			$css = $lang,
 			$cssVal = $css.text(),
-			$d = $cssVal
+			$b = $cssVal
 			.replace('{','<span class="bracket-op">{</span>')
 			.replace('}','<span class="bracket-cl">}</span>')
 			.replace('.','<span class="dot">.</span>')
 			.replace(':','<span class="db-dots">:</span>');
 
-			$css.empty().html($d);
+			$css.empty().html($b);
 
-		}		
+
+			//Javascript syntax
+			var $lang = $('pre.bulltax code.javascript');
+			$js = $lang,
+			$jsVal = $js.text(),
+			$c = $jsVal
+			.replace('{','<span class="bracket-op">{</span>')
+			.replace('}','<span class="bracket-cl">}</span>')
+			.replace('.','<span class="dot">.</span>')
+			.replace(':','<span class="db-dots">:</span>');
+
+			$js.empty().html($c);
+		
+
 	}
 }
 
